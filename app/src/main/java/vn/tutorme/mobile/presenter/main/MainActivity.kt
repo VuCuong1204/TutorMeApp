6a733a750c4c091942dc558e829221601d9015b2
cuongvq <cuongvq@ftech.ai>
2023-09-17 23:48:06 +0700

    add splash

--- app/src/main/java/vn/tutorme/mobile/presenter/main/MainActivity.kt
+++ app/src/main/java/vn/tutorme/mobile/presenter/main/MainActivity.kt
@@ -2,8 +2,11 @@ package vn.tutorme.mobile.presenter.main
 
 import dagger.hilt.android.AndroidEntryPoint
 import vn.tutorme.mobile.R
+import vn.tutorme.mobile.base.extension.gone
+import vn.tutorme.mobile.base.extension.show
 import vn.tutorme.mobile.base.screen.TutorMeActivity
 import vn.tutorme.mobile.databinding.MainActivityBinding
+import vn.tutorme.mobile.presenter.splash.SplashFragment
 
 @AndroidEntryPoint
 class MainActivity : TutorMeActivity<MainActivityBinding>(R.layout.main_activity) {
@@ -12,11 +15,13 @@ class MainActivity : TutorMeActivity<MainActivityBinding>(R.layout.main_activity
         super.onInitView()
     }
 
+    override fun getContainerId(): Int = R.id.flMainRoot
+
     override fun showLoading() {
-//        TODO("Not yet implemented")
+        binding.icMainLoading.show()
     }
 
     override fun hideLoading() {
-//        TODO("Not yet implemented")
+        binding.icMainLoading.gone()
     }
 }
